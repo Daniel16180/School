51f5c9cea1a49b4df3b7aa5ca3a213e3e4d9b883
Daniel Alegria <daniel.alegria@neoris.com>
2022-07-13 13:19:12 +0200

    TeacherRepository separated from Queries. Still not implemented

--- SchoolManagement/Repositories/TeacherRepository.cs
+++ SchoolManagement/Repositories/TeacherRepository.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Linq;
 using SchoolManagement;
 using SchoolManagementEntity;
+using SchoolManagement.Entities.DTO;
 
 namespace SchoolManagement.Repositories
 {
@@ -38,6 +39,33 @@ namespace SchoolManagement.Repositories
             }
         }
 
+        public void UpdateTeacher(int idTeacher, float salary, int exp)
+        {
+            using (IDbConnection conexion = new SqlConnection(connectionStr))
+            {
+                conexion.Open();
+
+                string sqlQuery = @"UPDATE Teacher
+                                    SET salary= " + salary + ", experience = " + exp + " " +
+                                    "WHERE id= " + idTeacher;
+
+                int rowsAffected = conexion.Execute(sqlQuery);
+            }
+        }
+
+        public void DeleteTeacher(int idTeacher)
+        {
+            using (IDbConnection conexion = new SqlConnection(connectionStr))
+            {
+                conexion.Open();
+
+                string sqlQuery = @"DELETE FROM Teacher
+                                    WHERE id = " + idTeacher;
+
+                int rowsAffected = conexion.Execute(sqlQuery);
+            }
+        }
+
         public void SetAssignment(int idClasgroup, int idTeacher)
         {
             using (IDbConnection conexion = new SqlConnection(connectionStr))
@@ -47,6 +75,70 @@ namespace SchoolManagement.Repositories
                 string sqlQuery = "INSERT INTO Classgroup_Teacher (id_classgroup, id_teacher) VALUES(" + idClasgroup + ", " + idTeacher + ")";
 
                 int rowsAffected = conexion.Execute(sqlQuery);
+            }
+        }
+
+        public void unassignTeachers(int fg)
+        { //fg=first group, the one without pupils
+            using (IDbConnection conexion = new SqlConnection(connectionStr))
+            {
+                conexion.Open();
+
+                string sqlQuery = "DELETE FROM Classgroup_Teacher WHERE id_classgroup = " + fg;
+
+                int rowsAffected = conexion.Execute(sqlQuery);
+            }
+        }
+
+        public void setDirector(int winsPos)
+        { //fg=first group, the one without pupils
+            using (IDbConnection conexion = new SqlConnection(connectionStr))
+            {
+                conexion.Open();
+
+                string sqlQuery = "UPDATE dbo.Teacher SET director = 'YES' WHERE id = " + winsPos;
+
+                int rowsAffected = conexion.Execute(sqlQuery);
+            }
+        }
+
+        public void unsetDirector()
+        { //fg=first group, the one without pupils
+            using (IDbConnection conexion = new SqlConnection(connectionStr))
+            {
+                conexion.Open();
+
+                string sqlQuery = "UPDATE dbo.Teacher SET director = 'NO'";
+
+                int rowsAffected = conexion.Execute(sqlQuery);
+            }
+        }
+
+        public IReadOnlyList<Person2DTO> findUnassignTeachers(int fg)
+        { //fg=first group, the one without pupils
+            using (IDbConnection conexion = new SqlConnection(connectionStr))
+            {
+                conexion.Open();
+
+                var Person2DTOList = conexion.Query<Person2DTO>(@"SELECT dbo.Teacher.id as Id, dbo.Teacher.first_name as Name, dbo.Teacher.last_name as Surname
+                                  FROM dbo.Teacher, dbo.Classgroup_Teacher
+                                  WHERE dbo.Classgroup_Teacher.id_teacher = dbo.Teacher.id AND  dbo.Classgroup_Teacher.id_classgroup = " + fg);
+
+                return Person2DTOList.ToList().AsReadOnly();
+            }
+        }
+
+        public IReadOnlyList<PersonDTO> MyTeachers(int idPupil)
+        {
+            using (IDbConnection conexion = new SqlConnection(connectionStr))
+            {
+                conexion.Open();
+
+
+                var personDTOList = conexion.Query<PersonDTO>(@"SELECT dbo.Teacher.first_name as Name, dbo.Teacher.last_name as Surname 
+                                                                FROM dbo.Teacher, dbo.Classgroup_Teacher, dbo.Pupil
+                                                                WHERE dbo.Teacher.id = dbo.Classgroup_Teacher.id_teacher AND dbo.Classgroup_Teacher.id_classgroup = dbo.Pupil.id_classgroup AND dbo.Pupil.id =" + idPupil);
+                return personDTOList.ToList().AsReadOnly();
             }
         }
     }
