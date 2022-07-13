8034916fe883caa991df1ddaca0d0e8cce8549a9
Daniel Alegria <daniel.alegria@neoris.com>
2022-07-13 13:39:22 +0200

    Pupil and clasgroup repositories added. Not implemented

---
+++ SchoolManagement/Repositories/ClassgroupRepository.cs
@@ -0,0 +1,81 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Linq;
+using SchoolManagement;
+using SchoolManagementEntity;
+using SchoolManagement.Entities.DTO;
+
+namespace SchoolManagement.Repositories
+{
+    class ClassgroupRepository
+    {
+        private string connectionStr = Initialize.GetConnectionString();
+
+        public IEnumerable<Classgroup> GetClassgroups()
+        {
+            using (IDbConnection conexion = new SqlConnection(connectionStr))
+            {
+                conexion.Open();
+
+                var classgroup = conexion.Query<Classgroup>("SELECT id as Id, year as Year, letter as Letter FROM Classgroup");
+                return classgroup;
+            }
+        }
+
+        public void SetClassgroup(int year, string letter)
+        {
+            using (IDbConnection conexion = new SqlConnection(connectionStr))
+            {
+                conexion.Open();
+
+                string sqlQuery = "INSERT INTO Classgroup (year, letter) VALUES(" + year + ", '" + letter + "')";
+
+                int rowsAffected = conexion.Execute(sqlQuery);
+            }
+        }
+
+        public void UpdateClassgroup(Classgroup c)
+        {
+            using (IDbConnection conexion = new SqlConnection(connectionStr))
+            {
+                conexion.Open();
+
+                string sqlQuery = @"UPDATE Classgroup
+                                    SET year= " + c.Year + ", letter= '" + c.Letter + "' " +
+                                    "WHERE id= " + c.Id;
+
+                int rowsAffected = conexion.Execute(sqlQuery);
+            }
+        }
+
+        public void DeleteClassgroup(int identification)
+        {
+            using (IDbConnection conexion = new SqlConnection(connectionStr))
+            {
+                conexion.Open();
+
+                string sqlQuery = @"DELETE FROM Classgroup
+                                    WHERE id = " + identification;
+
+                int rowsAffected = conexion.Execute(sqlQuery);
+            }
+        }
+
+        public void Merge(int firstGroup, int segondGroup)
+        {
+            using (IDbConnection conexion = new SqlConnection(connectionStr))
+            {
+                conexion.Open();
+
+                string sqlQuery = "UPDATE Pupil SET id_classgroup = " + segondGroup + " WHERE id_classgroup = " + firstGroup;
+
+                int rowsAffected = conexion.Execute(sqlQuery);
+            }
+        }
+
+    }
+}
---
+++ SchoolManagement/Repositories/PupilRepository.cs
@@ -0,0 +1,82 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Linq;
+using SchoolManagement;
+using SchoolManagementEntity;
+using SchoolManagement.Entities.DTO;
+
+namespace SchoolManagement.Repositories
+{
+    class PupilRepository
+    {
+        private string connectionStr = Initialize.GetConnectionString();
+
+        public IEnumerable<Pupil> GetPupils()
+        {
+            using (IDbConnection conexion = new SqlConnection(connectionStr))
+            {
+                conexion.Open();
+
+                var pupil = conexion.Query<Pupil>("SELECT id as Id, first_name as Name, last_name as Surname, age as Age, id_classgroup as IdClassgroup FROM Pupil");
+                return pupil;
+            }
+        }
+
+        public void SetPupil(Pupil p)
+        {
+            using (IDbConnection conexion = new SqlConnection(connectionStr))
+            {
+                conexion.Open();
+
+                string sqlQuery = "INSERT INTO Pupil (first_name, last_name, age, id_classgroup) VALUES(@Name, @Surname, @Age, @IdClassgroup)";
+
+                int rowsAffected = conexion.Execute(sqlQuery, p);
+            }
+        }
+
+        public void UpdatePupil(int idPupil, int age, int classId)
+        {
+            using (IDbConnection conexion = new SqlConnection(connectionStr))
+            {
+                conexion.Open();
+
+                string sqlQuery = @"UPDATE Pupil
+                                    SET age= " + age + ", id_classgroup = " + classId + " " +
+                                    "WHERE id= " + idPupil;
+
+                int rowsAffected = conexion.Execute(sqlQuery);
+            }
+        }
+
+        public void DeletePupil(int idPupil)
+        {
+            using (IDbConnection conexion = new SqlConnection(connectionStr))
+            {
+                conexion.Open();
+
+                string sqlQuery = @"DELETE FROM Pupil
+                                    WHERE id = " + idPupil;
+
+                int rowsAffected = conexion.Execute(sqlQuery);
+            }
+        }
+
+        public IReadOnlyList<Person2DTO> MyMates(int idClass)
+        {
+            using (IDbConnection conexion = new SqlConnection(connectionStr))
+            {
+                conexion.Open();
+
+
+                var personDTOList = conexion.Query<Person2DTO>(@"SELECT id as Id, first_name as Name, last_name as Surname FROM dbo.Pupil WHERE id_classgroup = " + idClass);
+                return personDTOList.ToList().AsReadOnly();
+            }
+        }
+
+
+    }
+}
